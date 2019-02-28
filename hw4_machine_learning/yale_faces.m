a6b83da5a64a3cc0e1927c0869988a5bf06e20d2
hengjiwang <hengjw@uw.edu>
2019-02-27 16:11:36 -0800

    start hw4

---
+++ hw4_machine_learning/yale_faces.m
@@ -0,0 +1,144 @@
+%% Load images
+
+close all; clear all; clc
+
+ims = [];
+
+for i = 1:39
+    if i<10
+        path = strcat('CroppedYale/yaleB0',num2str(i),'/');
+    else
+        path = strcat('CroppedYale/yaleB',num2str(i),'/');
+    end
+    
+    files = dir(strcat(path,'*.pgm'));
+    for j = 1:length(files)
+        im = imread(strcat(path,files(j).name));
+        im = reshape(im, [], 1);
+        ims = [ims, im];
+    end
+end
+
+ims = double(ims);
+
+%% Demean data
+
+[npixel, nimage] = size(ims);
+M = repmat(mean(ims,1),npixel,1);
+ims = ims - M;
+
+%% SVD
+
+[u,s,v] = svd(ims, 'econ');
+
+%% Plot S
+
+energies = max(s,[],1);
+energies = energies/sum(energies);
+
+figure(1)
+subplot(1,2,1)
+plot(energies, 'ro--')
+xlabel('PC')
+ylabel('energy')
+title('Energies')
+
+subplot(1,2,2)
+plot(log(energies), 'ro--')
+xlabel('PC')
+ylabel('log(energy)')
+title('Log scale energies')
+
+%% Plot U
+
+figure(2)
+
+% plot top 12 principle components
+for j = 1:12
+    subplot(3,4,j)
+    eigenface = reshape(u(:,j),[192,168]);
+    pcolor(eigenface); shading flat; colormap gray; axis ij;
+end
+
+%% Plot V
+
+figure(2)
+
+% plot the composition of the top 12 components
+for j = 1:12
+    subplot(3,4,j)
+    comsposition = v(j,:);
+    plot(composition)
+end
+
+%% Load uncropped images
+
+ims_uncrop = [];
+
+path = 'yalefaces_uncropped/yalefaces/';
+
+for i = 1:15
+    if i < 10
+        files = dir(strcat(path, 'subject0', num2str(i), '.*'));
+    else 
+        files = dir(strcat(path, 'subject', num2str(i), '.*'));
+    end
+    for j = 1:length(files)
+        im = imread(strcat(path,files(j).name));
+        im = reshape(im, [], 1);
+        ims_uncrop = [ims_uncrop, im];
+    end
+end
+
+ims_uncrop = double(ims_uncrop);
+
+%% Demean uncropped data
+
+[npixel, nimage] = size(ims_uncrop);
+M = repmat(mean(ims_uncrop,1),npixel,1);
+ims_uncrop = ims_uncrop - M;
+
+%% SVD on uncropped data
+
+[u,s,v] = svd(ims_uncrop, 'econ');
+
+%% Plot S
+
+energies = max(s,[],1);
+energies = energies/sum(energies);
+
+figure(1)
+subplot(1,2,1)
+plot(energies, 'ro--')
+xlabel('PC')
+ylabel('energy')
+title('Energies')
+
+subplot(1,2,2)
+plot(log(energies), 'ro--')
+xlabel('PC')
+ylabel('log(energy)')
+title('Log scale energies')
+
+%% Plot U
+
+figure(2)
+
+% plot top 12 principle components
+for j = 1:12
+    subplot(3,4,j)
+    eigenface = reshape(u(:,j),[243,320]);
+    pcolor(eigenface); shading flat; colormap gray; axis ij;
+end
+
+%% Plot V
+
+figure(2)
+
+% plot the composition of the top 12 components
+for j = 1:12
+    subplot(3,4,j)
+    comsposition = v(j,:);
+    plot(composition)
+end
+
