df4ae5b8f088935142aeca25a631225507877f34
hengjiwang <hengjw@uw.edu>
2019-03-19 11:44:00 -0700

    add matrix

--- project_birdsong_classification/matlab/main2.m
+++ project_birdsong_classification/matlab/main2.m
@@ -2,17 +2,17 @@
 
 close all; clear all; clc;
 
-path = ['/home/hengji/Documents/amath582_winter_2019/'...
+path = ['/Users/hengjiwang/Documents/amath582_winter_2019/'...
     'project_birdsong_classification/british-birdsong-dataset/songs/'];
 list = dir(fullfile(path));
 
 nfile = size(list,1) - 2;
 
-meta = importfile(['/home/hengji/Documents/amath582_winter_2019/'...
+meta = importfile(['/Users/hengjiwang/Documents/amath582_winter_2019/'...
     'project_birdsong_classification/'...
     'british-birdsong-dataset/birdsong_metadata.csv']);
 
-%% Construct training/test set
+%% Feature extraction
 time_clip = 10;
 npick = 10;
 
@@ -57,7 +57,7 @@ for k = 3:(nfile+2)
 end
 delete(h)
 
-%% 
+%% Contruct training/validation/test datasets
 
 L = (length(genuses'));
 
@@ -67,47 +67,73 @@ data_set = data_set(:,inds);
 genuses = genuses(:,inds);
 cnames = cnames(:,inds);
 
-%% 
-
 train_set = data_set(:, 1:floor(0.8*L));
-test_set = data_set(:, ceil(0.8*L):end);
+test_set = data_set(:, ceil(0.8*L)+1:end);
 
 genuses_train = genuses(1:floor(0.8*L));
-genuses_test = genuses(ceil(0.8*L):end);
-
-%%
+genuses_test = genuses(ceil(0.8*L)+1:end);
 
 training = abs(train_set);
 testing = abs(test_set);
 
-%%
+%% Classification
 
-% LDA
-class = classify(testing', training', genuses_train');
-accuracy = sum(class==genuses_test')/length(class);
-
-%%
+% % LDA
+% class_lda = classify(testing', training', genuses_train');
+% accuracy = sum(class_lda==genuses_test')/length(class_lda);
 
 % Naive Bayes
 Mdl_cnb = fitcnb(training', genuses_train');
 class_cnb = predict(Mdl_cnb, testing');
 accuracy_cnb = sum(class_cnb==genuses_test')/length(class_cnb);
 
-%%
-
 % Random Forests
 Mdl_rf = TreeBagger(500, training', genuses_train');
 class_rf = predict(Mdl_rf, testing');
-accuracy_rf = sum(class_rf==genuses_test')/length(class_rf);
 
+vtruth_rf = class_rf==genuses_test';
+accuracy_rf = sum(vtruth_rf)/length(class_rf);
 
 % KNN
 Mdl_knn = fitcknn(training', genuses_train');
 class_knn = predict(Mdl_knn, testing');
-accuracy_knn = sum(class_knn==genuses_test')/length(class_knn);
+
+vtruth_knn = class_knn==genuses_test';
+accuracy_knn = sum(vtruth_knn)/length(class_knn);
 
 
-% fit ensembles
-Mdl_ens = fitcensemble(training', genuses_train');
-class_ens = predict(Mdl_ens, testing');
-accuracy_ens = sum(class_ens==genuses_test')/length(class_ens);
+%% Results analysis
+
+list_genus = unique(genuses);
+list_cname = unique(cnames);
+
+count_genus = [];
+
+for genus = list_genus
+    count = length(find(genuses_test==genus));
+    count_genus = [count_genus; count];    
+end
+
+mtruth_genus = zeros(length(count_genus), length(count_genus));
+
+for j = 1:length(class_knn)
+    if vtruth_knn(j) == 1
+        i = find(list_genus==class_knn(j));
+        mtruth_genus(i, i) = mtruth_genus(i, i) + 1; 
+    else
+        i1 = find(list_genus==class_knn(j));
+        i2 = find(list_genus==genuses_test(j));
+        mtruth_genus(i2, i1) = mtruth_genus(i2, i1) + 1; 
+    end
+end
+
+for j = 1:length(mtruth_genus)
+    mtruth_genus(j,:) = mtruth_genus(j,:)/count_genus(j);
+end
+
+%% Plot results
+
+imshow(mtruth_genus)
+
+
+
